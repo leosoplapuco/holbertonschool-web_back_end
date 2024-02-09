ff519a4d2e5b447f3dc58880e7114944cb86a0f7
leosoplapuco <leosoplapuco29@gmail.com>
2024-02-09 11:06:07 -0500

    Fixed up: 2-hbtn_course - Correct documentation

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -32,7 +32,7 @@ export default class HolbertonCourse {
   set students(newStudents) {
     if (!Array.isArray(newStudents)) throw TypeError('students must be an Array');
     newStudents.forEach((student) => {
-        if (typeof student !== 'string') throw TypeError('student must be a String');
+      if (typeof student !== 'string') throw TypeError('student must be a String');
     });
     this._students = newStudents;
   }
