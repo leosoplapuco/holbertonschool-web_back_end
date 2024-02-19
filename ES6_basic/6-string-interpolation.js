563dcbde5031da40ff7255e4f7c258e887bd9ba1
leosoplapuco <leosoplapuco29@gmail.com>
2024-02-19 10:24:24 -0500

    Fixed Up: 6-string-interpolation - Correct export

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -1,4 +1,4 @@
-function getSanFranciscoDescription() {
+export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
     income: '$119,868',
@@ -8,5 +8,3 @@ function getSanFranciscoDescription() {
 
   return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
-
-module.exports = getSanFranciscoDescription;
